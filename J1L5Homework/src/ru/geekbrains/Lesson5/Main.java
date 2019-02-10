2d8f228aff602f368136651cc5a443a0c67089ed
Mr_Nicknacker <romik_slav@mail.ru>
2019-02-10 14:21:06 +0300

    J1L5 Introduction to OOP. Homework. Main Program

---
+++ J1L5Homework/src/ru/geekbrains/Lesson5/Main.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.Lesson5;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Person[] persArray = new Person[5];
+	persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "89231231212", 75000, 30);
+	persArray[1] = new Person("John Doe", "CEO", "JoD@mailbox.com", "89121231212", 350000, 57);
+	persArray[2] = new Person("Jane Doe", "deputy CEO", "JaD@mailbox.com", "89151231212", 250000, 43);
+	persArray[3] = new Person("Atemova Albina", "Secretary", "aalbina@mailbox.com", "8151234567", 65000, 24);
+	persArray[4] = new Person("Timurov Timur", "PR Director", "ttimur@mailbox.com", "89151234567", 120000, 35);
+
+		for (Person employee:persArray) {
+			employee.printPersonInfoOlderThan(40);
+			}
+		}
+}
